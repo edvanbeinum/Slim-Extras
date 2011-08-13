784405e405353c27bba0bf32b7bacf3c6fb1795f
Ed van Beinum <ed@vanbeinum.org>
2011-08-13 22:04:55 +0100

    Add Twig_Extension loader to TwigView

--- Views/TwigView.php
+++ Views/TwigView.php
@@ -37,19 +37,36 @@
  * Two fields that you, the developer, will need to change are:
  * - twigDirectory
  * - twigOptions
+ *
+ * If you want to write your own Twig Extensions then add the class names to the $twigExtensions array. You just need
+ * to add the class names as strings as they will be instantiated later by  TwigView::getEnvironment()
+ *
  */
-class TwigView extends Slim_View {
+class TwigView extends Slim_View
+{
 
     /**
      * @var string The path to the Twig code directory WITHOUT the trailing slash
      */
-    public static $twigDirectory = null;
+    public static $twigDirectory = 'Twig';
 
     /**
      * @var array The options for the Twig environment, see
      * http://www.twig-project.org/book/03-Twig-for-Developers
      */
     public static $twigOptions = array();
+
+    /**
+     * @var array Optional array of class names that implements the Twig_Extension Interface
+     * @see http://www.twig-project.org/doc/extensions.html
+     */
+    public static $twigExtensions = array();
+
+    /**
+     * @var string If you are using your own Extension(s) then put the path to the folder where there are saved
+     * relative to the directory where Slim.php is.
+     */
+    public static $twigExtensionsDirectory = array();
 
     /**
      * @var TwigEnvironment The Twig environment for rendering templates.
@@ -64,7 +81,8 @@ class TwigView extends Slim_View {
      * @param   string $template The path to the Twig template, relative to the Twig templates directory.
      * @return  void
      */
-    public function render( $template ) {
+    public function render( $template )
+    {
         $env = $this->getEnvironment();
         $template = $env->loadTemplate($template);
         return $template->render($this->data);
@@ -75,15 +93,29 @@ class TwigView extends Slim_View {
      *
      * @return TwigEnvironment
      */
-    private function getEnvironment() {
+    public function getEnvironment()
+    {
         if ( !$this->twigEnvironment ) {
             require_once self::$twigDirectory . '/Autoloader.php';
             Twig_Autoloader::register();
-            $loader = new Twig_Loader_Filesystem($this->getTemplatesDirectory());
+            $loader = new Twig_Loader_Filesystem( $this->getTemplatesDirectory() );
             $this->twigEnvironment = new Twig_Environment(
                 $loader,
                 self::$twigOptions
             );
+            
+            if ( !empty( self::$twigExtensions ) ) {
+                foreach ( self::$twigExtensions as $twigExtension ) {
+                    if ( !is_dir(self::$twigExtensionsDirectory) ) {
+                        throw new RuntimeException(
+                            "The directory for Twig Extention classes doesn't exist at {self::$twigExtensionsDirectory}"
+                        );
+                    }
+                    require_once self::$twigExtensionsDirectory . DIRECTORY_SEPARATOR . $twigExtension;
+                    $this->twigEnvironment->addExtension(new $twigExtension);
+                }
+
+            }
         }
         return $this->twigEnvironment;
     }
